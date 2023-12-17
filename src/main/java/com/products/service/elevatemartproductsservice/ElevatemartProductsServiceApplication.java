f1c0e164c7c766111d2e0a62d8b17881eb49d92e
shoaib-ahmed16 <shoaib-ahmed16>
2023-12-17 10:26:01 +0530

    pushing the changes with project files

---
+++ src/main/java/com/products/service/elevatemartproductsservice/ElevatemartProductsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.products.service.elevatemartproductsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatemartProductsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatemartProductsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/products/service/elevatemartproductsservice/ElevatemartProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.products.service.elevatemartproductsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatemartProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
